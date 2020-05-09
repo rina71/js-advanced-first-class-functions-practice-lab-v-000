27c7a602a41588d1fee17a82a5b58247429ec060
Yurina Shin <yurinas@gmail.com>
2020-05-09 02:42:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution in this file!
+function logDriverNames(array){
+  array.forEach(function(driver){
+    console.log(driver.name)
+  })
+}
+
+function logDriversByHometown(array, city){
+  array.forEach(function(driver){
+    if (driver.hometown === city)
+      console.log(driver.name)
+  })
+}
+
+const driversByRevenue = function(drivers){
+  let newArray = drivers.slice(0)
+  newArray.sort(function(a,b){
+    return a.revenue - b.revenue;
+  })
+  return newArray
+}
+
+const driversByName = function(drivers){
+  let newArray = drivers.slice(0)
+  newArray.sort(function(a,b){
+    return a.name.localeCompare(b.name)
+  })
+  return newArray
+}
